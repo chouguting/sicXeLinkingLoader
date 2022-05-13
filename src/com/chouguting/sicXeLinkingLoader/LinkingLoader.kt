55282cd91288f5f81d42dc90d92ed0f4d475e500
Gordon Chou <chouguting@gmail.com>
2022-05-13 20:18:12 +0800

    clean up

--- src/com/chouguting/sicXeLinkingLoader/LinkingLoader.kt
+++ src/com/chouguting/sicXeLinkingLoader/LinkingLoader.kt
@@ -1,36 +1,36 @@
 package com.chouguting.sicXeLinkingLoader
 
-class LinkingLoader(val memory: Memory,val allObjFilesStringList:List<List<String>>) {
-    fun linkAndLoad(){
+class LinkingLoader(val memory: Memory, val allObjFilesStringList: List<List<String>>) {
+    fun linkAndLoad() {
         val externalSymbolTable: MutableMap<String, Int> = mutableMapOf()
         val programAddress = memory.startAddressDecimal
         var controlSectionAddress = programAddress
 
         println("programAddress$programAddress")
         //pass 1
-        for(controlSectionStringList in allObjFilesStringList){
+        for (controlSectionStringList in allObjFilesStringList) {
             var controlSectionLength = 0
-            for (currentObjLine in controlSectionStringList){
-                if(currentObjLine.startsWith("H")){
-                    controlSectionLength = currentObjLine.substring(13,19).toDecimalInt() //程式長度
-                    val controlSectionName = currentObjLine.substring(1,7).trim()
-                    if(externalSymbolTable[controlSectionName] !=null){
+            for (currentObjLine in controlSectionStringList) {
+                if (currentObjLine.startsWith("H")) {
+                    controlSectionLength = currentObjLine.substring(13, 19).toDecimalInt() //程式長度
+                    val controlSectionName = currentObjLine.substring(1, 7).trim()
+                    if (externalSymbolTable[controlSectionName] != null) {
                         println("有重複的程式")
                         return
                     }
                     externalSymbolTable[controlSectionName] = controlSectionAddress
-                }else if(currentObjLine.startsWith("E")){
+                } else if (currentObjLine.startsWith("E")) {
                     break
-                }else if(currentObjLine.startsWith("D")){
+                } else if (currentObjLine.startsWith("D")) {
                     var currentDRecord = currentObjLine.drop(1)
-                    while(currentDRecord.isNotBlank()){
-                        val externalSymbolName = currentDRecord.substring(0,6).trim()
-                        val externalSymbolRelativeAddress = currentDRecord.substring(6,12).trimStart().toDecimalInt()
-                        if(externalSymbolTable[externalSymbolName] !=null){
+                    while (currentDRecord.isNotBlank()) {
+                        val externalSymbolName = currentDRecord.substring(0, 6).trim()
+                        val externalSymbolRelativeAddress = currentDRecord.substring(6, 12).trimStart().toDecimalInt()
+                        if (externalSymbolTable[externalSymbolName] != null) {
                             println("有重複的externalSymbol定義")
                             return
                         }
-                        externalSymbolTable[externalSymbolName] = externalSymbolRelativeAddress+controlSectionAddress
+                        externalSymbolTable[externalSymbolName] = externalSymbolRelativeAddress + controlSectionAddress
                         currentDRecord = currentDRecord.drop(12)
                     }
 
@@ -40,46 +40,46 @@ class LinkingLoader(val memory: Memory,val allObjFilesStringList:List<List<Strin
             controlSectionAddress += controlSectionLength
         }
 
-        val programLength = controlSectionAddress-programAddress
+        val programLength = controlSectionAddress - programAddress
         memory.createSpace(programLength)
         println("programLength:${programLength}")
 
         println(externalSymbolTable.toExternalSymbolTableString())
 
 
-
         //pass 2
         controlSectionAddress = programAddress
         var executionAddress = programAddress //先不要
-        for(controlSectionStringList in allObjFilesStringList){
+        for (controlSectionStringList in allObjFilesStringList) {
             var controlSectionLength = 0
-            for (currentObjLine in controlSectionStringList){
-                if(currentObjLine.startsWith("H")){
-                    controlSectionLength = currentObjLine.substring(13,19).toDecimalInt() //程式長度
-                }else if(currentObjLine.startsWith("E")){
-                    if(currentObjLine.length >= 7){
-                        executionAddress = currentObjLine.substring(1,7).toDecimalInt()+controlSectionAddress
+            for (currentObjLine in controlSectionStringList) {
+                if (currentObjLine.startsWith("H")) {
+                    controlSectionLength = currentObjLine.substring(13, 19).toDecimalInt() //程式長度
+                } else if (currentObjLine.startsWith("E")) {
+                    if (currentObjLine.length >= 7) {
+                        executionAddress = currentObjLine.substring(1, 7).toDecimalInt() + controlSectionAddress
                     }
                     break
-                }else if(currentObjLine.startsWith("T")){
+                } else if (currentObjLine.startsWith("T")) {
                     var currentRecord = currentObjLine
-                    val recordStartingAddress = currentRecord.substring(1,7).toDecimalInt() + controlSectionAddress
-                    val recordLength = currentRecord.substring(8,10).toDecimalInt()
+                    val recordStartingAddress = currentRecord.substring(1, 7).toDecimalInt() + controlSectionAddress
+                    val recordLength = currentRecord.substring(8, 10).toDecimalInt()
                     currentRecord = currentRecord.drop(9)
-                    memory.putTRecord(currentRecord,recordStartingAddress,recordLength)
-                }else if(currentObjLine.startsWith("M")){
-                    val modifyAddress = currentObjLine.substring(1,7).toDecimalInt() + controlSectionAddress
-                    val nibbleLength = currentObjLine.substring(7,9).toDecimalInt()
-                    val operation = if(currentObjLine[9]=='+') Memory.PLUS else Memory.MINUS
-                    val modifyValue = externalSymbolTable[currentObjLine.substring(10,currentObjLine.length).trim()] ?: 0
-                    memory.modifyMemory(operation,modifyValue,modifyAddress,nibbleLength)
+                    memory.putTRecord(currentRecord, recordStartingAddress, recordLength)
+                } else if (currentObjLine.startsWith("M")) {
+                    val modifyAddress = currentObjLine.substring(1, 7).toDecimalInt() + controlSectionAddress
+                    val nibbleLength = currentObjLine.substring(7, 9).toDecimalInt()
+                    val operation = if (currentObjLine[9] == '+') Memory.PLUS else Memory.MINUS
+                    val modifyValue =
+                        externalSymbolTable[currentObjLine.substring(10, currentObjLine.length).trim()] ?: 0
+                    memory.modifyMemory(operation, modifyValue, modifyAddress, nibbleLength)
                 }
 
             }
             controlSectionAddress += controlSectionLength
         }
         println(memory)
-        println("execution point: ${executionAddress.toHexString().padStart(4,'0')}")
+        println("execution point: ${executionAddress.toHexString().padStart(4, '0')}")
     }
 
 }
--- src/com/chouguting/sicXeLinkingLoader/Memory.kt
+++ src/com/chouguting/sicXeLinkingLoader/Memory.kt
@@ -3,111 +3,121 @@ package com.chouguting.sicXeLinkingLoader
 
 class Memory() {
 
-    companion object{
+    companion object {
         val PLUS = 1
         val MINUS = -1
     }
 
-    var startAddressDecimal:Int = 0
-    var endAddressDecimal:Int = 0
-    lateinit var memorySpace:Array<ByteString>
+    var startAddressDecimal: Int = 0
+    var endAddressDecimal: Int = 0
+    lateinit var memorySpace: Array<ByteString>
 
-    val startAddressHexString:String
+    val startAddressHexString: String
         get() {
             return Integer.toHexString(startAddressDecimal).uppercase()
         }
 
 
     //如果輸入16進位字串，就先轉成10進位
-    constructor(startAddressHexString:String) : this(){
+    constructor(startAddressHexString: String) : this() {
         startAddressDecimal = startAddressHexString.toInt(16)
     }
 
     override fun toString(): String {
         var str = "\nMemory(startAddress=$startAddressHexString)\n"
-        str+="address\t                      Contents\n"
-        str+="=======      =========================================\n"
+        str += "address\t                      Contents\n"
+        str += "=======      =========================================\n"
         var widthCounter = 0
         var locationCounter = startAddressDecimal
-        str+="${locationCounter.toHexString()}\t\t"
-        for(byte in memorySpace){
-            widthCounter+=1
-            locationCounter+=1
-            str+=byte
-            if(widthCounter==16){
-                str+="\n"
-                widthCounter=0
-                str+="${locationCounter.toHexString().uppercase()}\t\t"
-            }else if(widthCounter%4==0){
-                str+="\t"
+        str += "${locationCounter.toHexString()}\t\t"
+        for (byte in memorySpace) {
+            widthCounter += 1
+            locationCounter += 1
+            str += byte
+            if (widthCounter == 16) {
+                str += "\n"
+                widthCounter = 0
+                str += "${locationCounter.toHexString().uppercase()}\t\t"
+            } else if (widthCounter % 4 == 0) {
+                str += "\t"
             }
         }
         return str
     }
 
-    fun putTRecord(recordContent:String, address:Int, length:Int){
+    fun putTRecord(recordContent: String, address: Int, length: Int) {
         var currentRecord = recordContent
         var locationCounter = address - startAddressDecimal
-        while(currentRecord.isNotBlank()){
-            memorySpace[locationCounter] = ByteString(currentRecord.substring(0,2).uppercase())
+        while (currentRecord.isNotBlank()) {
+            memorySpace[locationCounter] = ByteString(currentRecord.substring(0, 2).uppercase())
             currentRecord = currentRecord.drop(2)
-            locationCounter+=1
+            locationCounter += 1
         }
     }
 
-    fun modifyMemory(operation:Int,modifyValue:Int,absoluteAddress:Int, nibbleLength:Int){
-        val address = absoluteAddress-startAddressDecimal
-        val actualModifyOffset = modifyValue*operation //加或減
-        val firstByteIsHalf = (nibbleLength%2 == 1)
-        var stringToBeModified = if(firstByteIsHalf) memorySpace[address].lowerNibble else ""
-        var locationCounter = if(firstByteIsHalf) address+1 else address
-        for(i in 0 until nibbleLength/2){
-            stringToBeModified+=memorySpace[locationCounter]
-            locationCounter+=1
+    fun modifyMemory(operation: Int, modifyValue: Int, absoluteAddress: Int, nibbleLength: Int) {
+        val address = absoluteAddress - startAddressDecimal
+        val actualModifyOffset = modifyValue * operation //加或減
+        val firstByteIsHalf = (nibbleLength % 2 == 1)
+        var stringToBeModified = if (firstByteIsHalf) memorySpace[address].lowerNibble else ""
+        var locationCounter = if (firstByteIsHalf) address + 1 else address
+        for (i in 0 until nibbleLength / 2) {
+            stringToBeModified += memorySpace[locationCounter]
+            locationCounter += 1
         }
         //TODO:判斷正負
-        val intToBeModified = if(stringToBeModified.hexIsNegative()) ((stringToBeModified.hexTwosComplement().toDecimalInt())*-1) else stringToBeModified.toDecimalInt()
+        val intToBeModified = if (stringToBeModified.hexIsNegative()) ((stringToBeModified.hexTwosComplement()
+            .toDecimalInt()) * -1) else stringToBeModified.toDecimalInt()
         //val intToBeModified = stringToBeModified.toDecimalInt()
         var modifiedResult = intToBeModified + actualModifyOffset
-        var resultString = modifiedResult.toHexString().uppercase().padStart(nibbleLength,'0')
-        if(resultString.length>nibbleLength){
-            resultString = resultString.drop(resultString.length-nibbleLength)
+        var resultString = modifiedResult.toHexString().uppercase().padStart(nibbleLength, '0')
+        if (resultString.length > nibbleLength) {
+            resultString = resultString.drop(resultString.length - nibbleLength)
         }
         locationCounter = address
-        if(firstByteIsHalf) {
+        if (firstByteIsHalf) {
             memorySpace[address].lowerNibble = resultString[0].toString()
-            locationCounter+=1
+            locationCounter += 1
             resultString = resultString.drop(1)
         }
-        for(i in 0 until nibbleLength/2){
-            memorySpace[locationCounter] =ByteString(resultString.substring(0,2))
-            resultString=resultString.drop(2)
-            locationCounter+=1
+        for (i in 0 until nibbleLength / 2) {
+            memorySpace[locationCounter] = ByteString(resultString.substring(0, 2))
+            resultString = resultString.drop(2)
+            locationCounter += 1
         }
     }
 
 
-    fun createSpace(spaceSize: Int){
-        memorySpace = Array(spaceSize){ ByteString("**") }
+    fun createSpace(spaceSize: Int) {
+        memorySpace = Array(spaceSize) { ByteString("**") }
     }
 
 
 }
 
-class ByteString(){
+class ByteString() {
     var higherNibble = "0"
     var lowerNibble = "0"
-    constructor(byteHexString:String): this(){
+
+    constructor(byteHexString: String) : this() {
         setByte(byteHexString)
     }
 
-    fun setByte(byteHexString:String){
-        higherNibble = if(byteHexString[0]=='*'){ "*" }else{ byteHexString[0].uppercase() }
-        lowerNibble = if(byteHexString[1]=='*'){ "*" }else{ byteHexString[1].uppercase() }
+    fun setByte(byteHexString: String) {
+        higherNibble = if (byteHexString[0] == '*') {
+            "*"
+        } else {
+            byteHexString[0].uppercase()
+        }
+        lowerNibble = if (byteHexString[1] == '*') {
+            "*"
+        } else {
+            byteHexString[1].uppercase()
+        }
     }
 
     override fun toString(): String {
-        return higherNibble+lowerNibble
+        return higherNibble + lowerNibble
     }
 
 
