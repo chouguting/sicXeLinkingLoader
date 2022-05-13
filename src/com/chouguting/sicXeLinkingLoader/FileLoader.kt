0d987c83cb259761a1e009f0482639c3942369c6
Gordon Chou <chouguting@gmail.com>
2022-05-13 23:40:26 +0800

    finish up

--- src/com/chouguting/sicXeLinkingLoader/FileLoader.kt
+++ src/com/chouguting/sicXeLinkingLoader/FileLoader.kt
@@ -11,9 +11,6 @@ class FileLoader(private val fileName: String) {
             inputLines = File(fileName).useLines { it.toList() }
         } catch (e: FileNotFoundException) {
             println("File not found" + e)
-            println("找不到指定的檔案(如果未指定檔案預設是會讀入 input.asm)")
-            println("請特別注意 txt檔要放在和jar檔同一個資料夾下")
-            println("執行時要在資料夾外輸入指令 : java -jar sicAssembler.jar input.asm")
             exitProcess(-1)
         }
         return inputLines
